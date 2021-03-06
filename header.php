4b32feaedb02d06f819b808f2aff5ee9f6d079a2
Nurul Rossa <50910678+nurulrossa@users.noreply.github.com>
2021-03-06 19:48:04 +0700

    Hapus bagian bootstrap & fontawesome
    baris 14-16

--- header.php
+++ header.php
@@ -11,9 +11,7 @@
 		<meta name="description" content="overview &amp; stats" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-		<!-- bootstrap & fontawesome -->
-		<link rel="stylesheet" href="<?=base_url()?>/assets/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="<?=base_url()?>/assets/font-awesome/4.5.0/css/font-awesome.min.css" />
+
 
 		<!-- page specific plugin styles -->
 
